2abc4d4ce3c69c2f10a5c075696eca301e44b003
Jakub Cembrzyński <kubacembrzynski97@o2.pl>
2019-05-10 18:18:10 +0200

    Finished comparasion of 4 datasets and 4 classifiers

--- Dataset.py
+++ Dataset.py
@@ -1,5 +1,8 @@
 from numpy import genfromtxt
 from sklearn.model_selection import train_test_split
+from numpy import ndarray
+from random import sample
+from copy import deepcopy
 class Dataset:
     def __init__(self, path, name):
         data = genfromtxt(path, delimiter=',')
@@ -10,7 +13,13 @@ class Dataset:
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=0.3, random_state=0)
         self.name = name
 
-    def GetRandomSubspaceDataset(self):
-        data = Dataset()
-
+    def GetRandomSubspaceDataset(self, attNumber=3):
+        data = deepcopy(self)
+        attNumber = round(0.8*self.X.shape[1])
+        randomIndexes = sample(range(self.X.shape[1]),attNumber)
+        data.X = ndarray(shape=(self.X.shape[0],attNumber))
+        for i in range(attNumber):
+            data.X[:, i] = self.X[:, randomIndexes[i]]
+        data.y = self.y
+        data.X_train, data.X_test, data.y_train, data.y_test = train_test_split(data.X, data.y, test_size=0.3, random_state=0)
         return data
--- RandomSubspace.py
+++ RandomSubspace.py
@@ -1,13 +1,12 @@
-# Estimators
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
 from Dataset import Dataset
+from statistics import mean
 
-# Others
-from numpy import genfromtxt
-from sklearn.model_selection import train_test_split
+
+
 from sklearn.metrics import accuracy_score
 #Import data from files
 
@@ -16,7 +15,7 @@ datasets.append(Dataset("datasets\diabetes.csv", "Diabetes set"))
 datasets.append(Dataset("datasets\wine.csv", "Wine set"))
 datasets.append(Dataset("datasets\german.csv", "German set"))
 datasets.append(Dataset("datasets\popfailures.csv","Popfailures set"))
-"""
+
 #step 1 - full data approach
 scores = []
 
@@ -33,18 +32,49 @@ for i in range(len(datasets)):
         predictions.append(classifiers[j].predict(datasets[i].X_test))
         scores.append(accuracy_score(datasets[i].y_test, predictions[j]))
 
-print(datasets[0].name)
-print(scores[0:4])
-print(datasets[1].name)
-print(scores[4:8])
-print(datasets[2].name)
-print(scores[8:12])
-print(datasets[3].name)
-print(scores[12:16])
-"""
+
 #step 2 - random subspace approach
+means = []
+for d in range(len(datasets)):
+    subspaceDatasets = []
+    for i in range(10):
+        subspaceDatasets.append(datasets[d].GetRandomSubspaceDataset())
+    subspaceScores = []
 
+    for i in range(len(subspaceDatasets)):
+        classifiers = [
+            KNeighborsClassifier(),
+            GaussianNB(),
+            DecisionTreeClassifier(),
+            SVC(gamma='auto')
+        ]
+        predictions = []
+        for j in range(len(classifiers)):
+            classifiers[j].fit(subspaceDatasets[i].X_train, subspaceDatasets[i].y_train)
+            predictions.append(classifiers[j].predict(subspaceDatasets[i].X_test))
+            subspaceScores.append(accuracy_score(subspaceDatasets[i].y_test, predictions[j]))
+    for i in range(len(classifiers)):
+        means.append(mean(subspaceScores[i:(i+1*10)]))
 
-#divide random subspaces of datasets
-#calculate accuracy scores for each set
-#compare results: normal vs random subspace
+comparasion = []
+for i in range(len(scores)):
+    scores[i] = round(scores[i],2)
+    means[i] = round(means[i],2)
+    comparasion.append(round(scores[i]/means[i],2))
+
+for i in range(len(datasets)):
+    print("Results: for " + datasets[i].name)
+    print("Normal approach ")
+    print("KNN, GaussianNB, Decision Tree, SVC")
+    print(scores[i:i+4])
+    print("Subspace approach:")
+    print("KNN, GaussianNB, Decision Tree, SVC")
+    print(means[i:i+4])
+    print("Normal approach / Subspace approach")
+    print(comparasion[i:i + 4])
+    print("")
+counter = 0
+for i in range(len(comparasion)):
+    if comparasion[i] < 1:
+        counter = counter +1
+print("In " + str(counter) + " of " + str(len(comparasion)) + " tests achieved higher accuracy with random subspace")
